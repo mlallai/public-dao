761c9ef7e3b08d4ab9f2492ced12cec67a6a1318
mlallai <matthieu.lallai@gmail.com>
2024-11-14 16:17:03 +0100

    Add request log interceptor

--- public-dao-api/src/main.ts
+++ public-dao-api/src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './infrastructure/common/filter/exception.filter';
 import { LoggerService } from './infrastructure/logger/logger.service';
+import { LoggingInterceptor } from './infrastructure/interceptors/logger.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Filter
+  // Filters
   app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+
+  // Interceptors
+  app.useGlobalInterceptors(new LoggingInterceptor(new LoggerService()));
 
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3000);
